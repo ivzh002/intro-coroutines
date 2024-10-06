88d0b67af278d406bafe5ea577ba520b9216c5c5
sun_02 <i_v_zhukov@yahoo.com>
2024-10-06 14:29:29 +0300

    Task 4 changed to run suspend fun consequently. Completed task 5.

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -2,8 +2,20 @@ package tasks
 
 import contributors.*
 import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
 
+@OptIn(DelicateCoroutinesApi::class)
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    return repos.map { repo ->
+        GlobalScope.async(Dispatchers.IO) {
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }.awaitAll().flatten().aggregate()
 }
